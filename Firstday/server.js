9bb8190a987ae8905644bc72a19746fc63e67c8a
minseokim <85022613+algoriminseo@users.noreply.github.com>
2025-01-16 21:59:34 +0900

    Update server.js

--- Firstday/server.js
+++ Firstday/server.js
@@ -1,6 +1,10 @@
 const express = require('express') //express 라이브러리 사용
 const app = express()              //express 라이브러리 사용
 
+//폴더를 server.js에 등록해주면 폴더안의 파일들 html에서 사용가능
+//.css, .js, .jpg (모두 )
+app.use(express.static(__dirname + '/public'))
+
 //서버 띄우는 코드
 //내 컴퓨터 PORT 하나 오픈하는 문법
 app.listen(8080, () => {
@@ -13,16 +17,11 @@ app.get('/', (요청, 응답) => {
 })
 
 
-
 //누가 이런 URL을 접속하면
 app.get('/news', (요청, 응답) => {
     응답.send('오늘 비옴')
-})
+}) 
 
 app.get('/shop', (요청, 응답) => {
     응답.send('쇼핑페이지임')
-})
-
-app.get('/about', (요청, 응답) => {
-    응답.sendFile(__dirname + '/intro.html')
 })
